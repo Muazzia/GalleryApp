4821dba1224ce1aa8c72caa5ccccc4fb8ae57ea2
Muaz <you@example.com>
2023-12-29 20:46:28 +0500

    added file save

--- src/components/DetailPage.tsx
+++ src/components/DetailPage.tsx
@@ -1,12 +1,44 @@
-import { Box, Image, Spinner, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Select,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import { saveAs } from "file-saver";
 
 import usePhoto from "../hooks/usePhoto";
+import { Photo } from "../hooks/usePhotos";
+import { useState } from "react";
 
 const DetailPage = () => {
   const params = useParams();
   const id = params.id || "0";
   const { data, error, isLoading } = usePhoto(parseInt(id));
+
+  const downloadType: Photo["src"] = {
+    original: "original",
+    landscape: "landscape",
+    large: "large",
+    large2x: "large2x",
+    medium: "medium",
+    portrait: "portrait",
+    small: "small",
+  };
+  // const [downloadType, setDownloadType] = useState("");
+  const [type, setType] = useState<keyof Photo["src"]>("original");
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setType(e.target.value as "original");
+  };
+
+  const handleClick = () => {
+    let url = data?.src[type] || "";
+    saveAs(url, data?.alt);
+  };
 
   if (isLoading) return <Spinner />;
   if (error) return <Text color={"red"}>Image not found</Text>;
@@ -20,6 +52,18 @@ const DetailPage = () => {
           h={"80vh"}
           width={"90vw"}
         />
+        <Flex justifyContent={"center"} mt={4} gap={3}>
+          <Box>
+            <Select onChange={handleOnChange}>
+              {Object.keys(downloadType).map((k) => (
+                <option value={k}>{k}</option>
+              ))}
+            </Select>
+          </Box>
+          <Button onClick={handleClick} colorScheme="teal" size="md">
+            Download
+          </Button>
+        </Flex>
       </Box>
     );
   // throw error;
